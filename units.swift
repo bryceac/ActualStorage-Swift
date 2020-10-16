7bc1b7745e4bebfa27f6106a258a7fc01a1a0c75
Bryce <tonyhawk2100@gmail.com>
2020-10-16 16:47:54 -0700

    remove unnecessary file.

--- units.swift
+++
@@ -1,8 +0,0 @@
-// The Units struct is used to load units of measure for secondary storage from a JSON file
-struct Units: Codable {
-    let units: [String]
-
-    private enum CodingKeys: String, CodingKey {
-        case units
-    }
-}
