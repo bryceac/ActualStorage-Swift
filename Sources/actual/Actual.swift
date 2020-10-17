9581090626cccea87b8af86176b88472b30500ee
Bryce <tonyhawk2100@gmail.com>
2020-10-16 17:16:14 -0700

    use computed property to load variables.

--- Sources/actual/Actual.swift
+++ Sources/actual/Actual.swift
@@ -5,7 +5,14 @@ struct Actual: ParsableCommand {
     @Argument var size: Int
     @Argument var unit: String
 
+    var units: [String]? {
+        let JSON_DECODER = JSONDecoder()
+
+        guard let UNIT_URL = Bundle.module.url(forResource: "units", withExtension: "json"), let UNIT_DATA = try? Data(contentsOf: UNIT_URL), let UNITS = try? JSON_DECODER.decode([String].self, from: UNIT_DATA) else { return nil }
+
+        return UNITS
+    }
+
     func run() {
-        
     }
 }
