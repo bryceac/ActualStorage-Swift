824417d720ecbd6d9bcce71f07ffd26890d4a3ec
Bryce <tonyhawk2100@gmail.com>
2020-10-16 18:03:26 -0700

    fix issue with string comparison.

--- Sources/actual/Actual.swift
+++ Sources/actual/Actual.swift
@@ -32,7 +32,7 @@ struct Actual: ParsableCommand {
             throw CalculateError.unitTooLong
         }
 
-        guard let INDEX = units.firstIndex(where: { unit.count == 1 ? $0.prefix(1).localizedCaseInsensiveCompare(unit) == .orderedSame : $0.localizedCaseInsensiveCompare(unit) == .orderedSame }) else {
+        guard let INDEX = units.firstIndex(where: { unit.count == 1 ? unit.localizedCaseInsensiveCompare($0.prefix(1)) == .orderedSame : $0.localizedCaseInsensiveCompare(unit) == .orderedSame }) else {
             throw CalculateError.invalidUnit
         }
 
